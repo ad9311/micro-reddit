d23e3cd4638ae60934a938d76ade69238b363921
Ángel Díaz <diazangel9311@gmail.com>
2021-06-30 18:22:08 -0500

    Correct linter issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :comments
 
   validates :username, presence: true, uniqueness: true, length: { minimum: 6 }
-  validates :email, presence: true, uniqueness: true, format: {with: /[\w\-\.]+@[\w]+\.[a-z]+/, message: 'Incorrect email format'}
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: /[\w\-.]+@\w+\.[a-z]+/, message: 'Incorrect email format' }
   validates :password, presence: true, length: { in: 8..16 }
 end
